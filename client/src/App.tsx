60847c1525e00ffdb0cdc8b79eb22110b3d34526
SaniatAzam <sa.saniat@mail.mcgill.ca>
2024-05-04 18:30:33 +0600

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      className="text-red-500
+    "
+    >
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default App;
